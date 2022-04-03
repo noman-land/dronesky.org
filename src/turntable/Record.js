048635cf5f162cb3ce57b9f6d26664834bb5a598
Noman <noman@noman.land>
2022-04-03 00:44:18 -0400

    Remove unused function

--- src/turntable/Record.js
+++ src/turntable/Record.js
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 const RecordDiv = styled('div')`
