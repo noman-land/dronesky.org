ee579674b5373d3d5fa624e13103483cae4fef29
Noman <noman@noman.land>
2022-04-02 23:47:07 -0400

    Add semicolon

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 
 import { App } from './App';
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
